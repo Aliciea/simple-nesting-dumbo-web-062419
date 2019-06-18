d9ee08be336ef372bae57d18a33b7d859a44b7a8
Aliciea D Suechin <adr330@nyu.edu>
2019-06-18 18:55:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ def changing_alan
      :alans_new_info => {
        :known_for => "GUI"
      }
- 		end 
+ 	end 
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      end
